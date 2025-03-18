de8077e8c4dfa4b91f922563e32824b543c8ef4f
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-03-18 19:03:18 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,52 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sum2D(int[][] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				sum += arr[i][j];
+			}
+		}
+		return sum;
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
+	public static int sumRow(int[][] ARR, int num) {
 		//Write question 2 code here
-	}
+		int row = 0;
+		int[] lst = ARR[num];
+		for (int i = 0; i < lst.length; i++) {
+			row += lst[i];
+			}
+			return row;
+		}
+		
 
-	public static void q3() {
+
+	public static int sumColumn(int[][] arr1, int x) {
 		//Write question 3 code here
+		int column = 0;
+		
+		for (int i = 0; i < arr1.length; i++) {
+			column += arr1[i][x];
+			}
+			return column;
+		}
+		
+	
+
+	
+	public static void main(String args[]) {
+		int[][] result = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		System.out.println(sum2D(result));
+		
+		int[][] arrayNum = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		int result2 = sumRow(arrayNum, 0);
+		System.out.println(result2);
+
+		int[][] arrayList = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		int result3 = sumColumn(arrayList, 1);
+		System.out.println(result3);
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 }
